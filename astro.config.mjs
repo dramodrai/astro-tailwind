0cd310bc248b15d904472a33a358f5bc781fecc5
amod rai <dr.amodrai@gmail.com>
2022-08-14 22:48:45 +0530

    change in prose size

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,54 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import preact from "@astrojs/preact"; // import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import preact from '@astrojs/preact'; // import mdx from '@astrojs/mdx';
 // https://astro.build/config
 
-import vue from "@astrojs/vue";
-import sitemap from "@astrojs/sitemap";
-import mdx from "@astrojs/mdx";
-import partytown from "@astrojs/partytown";
+import vue from '@astrojs/vue';
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
 
-import image from "@astrojs/image";
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-tailwind-learn.netlify.app/',
-  integrations: [tailwind({
-    config: {
-      applyBaseStyles: false
-    }
-  }), preact(), vue(), sitemap(), mdx(), partytown(), image()],
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    preact(),
+    vue(),
+    sitemap(),
+    mdx(),
+    partytown(),
+    image(),
+  ],
   vite: {
     ssr: {
-      external: ["svgo"]
-    }
+      external: ['svgo'],
+    },
   },
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
-      langs: ['astro','vue','js','css','html','json','jsx','tsx','svelte'],
-      wrap: true
+      langs: [
+        'astro',
+        'vue',
+        'js',
+        'css',
+        'html',
+        'json',
+        'jsx',
+        'tsx',
+        'svelte',
+        'bash'
+      ],
+      wrap: true,
     },
     // syntaxHighlight: 'prism',
-    drafts: true
-  } 
+    drafts: true,
+  },
 });
