04239d134fc05b86832a300be54a1d5ad8032923
amod rai <dr.amodrai@gmail.com>
2022-09-02 10:52:20 +0530

    add rotots.txt

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,23 @@ module.exports = {
 	darkMode:'class',
 	content: ['./src/**/*.{astro,html,md,js,jsx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			'animation': {
+				'text':'text 5s ease infinite',
+			},
+			'keyframes': {
+				'text': {
+					'0%, 100%': {
+					   'background-size':'200% 200%',
+						'background-position': 'left center'
+					},
+					'50%': {
+					   'background-size':'200% 200%',
+						'background-position': 'right center'
+					}
+				},
+			}
+		},
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
